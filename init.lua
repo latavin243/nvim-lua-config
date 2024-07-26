753c50b892ecd025ca28e21cebd6a7fa67306463
qi.guo <qi.guo@shopee.com>
2024-07-26 15:11:49 +0800

    mod startify ignore sessions

--- init.lua
+++ init.lua
@@ -208,7 +208,7 @@ require("lazy").setup({
       config = function()
         vim.g.startify_change_to_dir = 0
         vim.g.startify_change_to_vcs_root = 1
-        vim.g.startify_session_dir = "$HOME/.vim/sessions"
+        -- vim.g.startify_session_dir = "$HOME/.config/nvim/"
         vim.g.startify_files_number = 20
       end
     },
